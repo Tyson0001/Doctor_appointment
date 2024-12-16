5f671b08d0754742769911199427a91bf55537ee
me <namansoniop@gmail.com>
2024-12-16 15:00:06 +0530

    UI designing of Registration page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,55 +4,80 @@ import { Form, Input, message } from "antd";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { showLoading,hideLoading } from "../redux/features/alertSlice";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+
 const Register = () => {
   const navigate = useNavigate();
-  const dispatch=useDispatch()
-  //form handler
-  const onfinishHandler = async (values) => {
+  const dispatch = useDispatch();
+
+  // form handler
+  const onFinishHandler = async (values) => {
     try {
-      dispatch(showLoading())
+      dispatch(showLoading());
       const res = await axios.post("/api/v1/user/register", values);
-      dispatch(hideLoading())
+      dispatch(hideLoading());
       if (res.data.success) {
-        message.success("Register Successfully!");
+        message.success("Registered Successfully!");
         navigate("/login");
       } else {
         message.error(res.data.message);
       }
     } catch (error) {
-      dispatch(hideLoading())
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something Went Wrong");
     }
   };
+
   return (
-    <>
-      <div className="form-container ">
-        <Form
-          layout="vertical"
-          onFinish={onfinishHandler}
-          className="register-form"
-        >
-          <h3 className="text-center">Register From</h3>
-          <Form.Item label="Name" name="name">
-            <Input type="text" required />
-          </Form.Item>
-          <Form.Item label="Email" name="email">
-            <Input type="email" required />
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input type="password" required />
-          </Form.Item>
-          <Link to="/login" className="m-2">
-            Already user login here
-          </Link>
-          <button className="btn btn-primary" type="submit">
-            Register
-          </button>
-        </Form>
-      </div>
-    </>
+    <div className="form-container">
+      {/* Header Section */}
+      <header className="header-section">
+        <div className="logo-container">
+          <img src="/logo.png" alt="IIT Jodhpur Logo" className="logo-image" />
+          <h1 className="heading">Health Centre - Indian Institute Of Technology Jodhpur</h1>
+        </div>
+        <hr className="header-divider" />
+      </header>
+
+      {/* Registration Form Section */}
+      <main className="form-section">
+        <div className="form-wrapper">
+          <h2 className="form-title2">Register to Your Account</h2>
+          <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
+            {/* Name Field */}
+            <Form.Item label="Name" name="name" rules={[{ required: true, message: "Please enter your name!" }]}>
+              <Input type="text" placeholder="Enter your name" />
+            </Form.Item>
+
+            {/* Email Field */}
+            <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please enter your email!" }, { type: "email", message: "Please enter a valid email!" }]}>
+              <Input type="email" placeholder="Enter your email" />
+            </Form.Item>
+
+            {/* Password Field */}
+            <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please enter your password!" }]}>
+              <Input.Password placeholder="Enter your password" />
+            </Form.Item>
+
+            {/* Register Button */}
+            <button className="btn btn-primary" type="submit">
+              Register
+            </button>
+          </Form>
+
+          {/* Additional Link */}
+          <div className="extra-links">
+            <Link to="/login">Already a user? Login here</Link>
+          </div>
+        </div>
+      </main>
+
+      {/* Footer Section */}
+      <footer className="footer-section">
+        <p className="footer-text">© {new Date().getFullYear()} Indian Institute of Technology Jodhpur</p>
+      </footer>
+    </div>
   );
 };
 
